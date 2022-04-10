ef0a24e9dc6c6bb342739914472f7484c06035ff
Lutz Herrmann <lutz.herr@gmail.com>
2022-04-10 12:36:10 +0200

    Sportfestserver - Login

--- sportcompetition/urls.py
+++ sportcompetition/urls.py
@@ -1,6 +1,12 @@
+from unicodedata import name
 from django.urls import path
 from . import views
+import sportcompetition
 
+app_name = 'sportcompetition'
 urlpatterns = [
     path('', views.main, name='main'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
+
--- sportcompetition/views.py
+++ sportcompetition/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.urls.base import reverse
 
 # Create your views here.
 
 def main(request):
     return render(request, 'sportcompetition/main.html', {})
+
+def login_view(request):
+    login_form = AuthenticationForm()
+    if request.method == "POST":
+        login_form = AuthenticationForm(data=request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+    return render(request, 'sportcompetition/login.html', {'login_form': login_form})
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('sportcompetition:main'))
